fdd3459f16e3b7232fd2c138997b3288c1cc3f3e
GritDim <gritsun.d@gmail.com>
2022-04-28 17:48:41 +0300

    Добавьте файлы проекта.

---
+++ Class.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_13
+{
+    class Building
+    {
+        public int Adres { get; set; }
+        public int Dlina { get; set; }
+        public int Vysota { get; set; }
+        public int Shirina { get; set; }
+
+        public Building(int adres, int dlina, int vysota, int shirina)
+        {
+            Adres = adres;
+            Dlina = dlina;
+            Vysota = vysota;
+            Shirina = shirina;
+        }
+        public string GetInfo()
+        {
+            return $"Адрес - {Adres} \n Длина здания - {Dlina}\n Высота здния - {Vysota}\n Ширина здания - {Shirina}";
+        }
+
+    }
+
+    sealed class MultiBuilding:Building
+    {
+        public int Etagi { get; set; }
+        public MultiBuilding(int adres, int dlina, int vysota, int shirina, int etagi)
+            : base(adres, dlina, vysota, shirina)
+        {
+            Etagi = etagi;
+        }
+
+        public string GetInfo()
+        {
+            string result = base.GetInfo();
+            return $"{result}\n Этажность здания - {Etagi}";
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Building building = new Building(10, 10, 10, 10);
+            MultiBuilding multiBuilding = new MultiBuilding(20, 20, 20, 20, 5);
+            Console.WriteLine(building.GetInfo());
+            Console.WriteLine(multiBuilding.GetInfo());
+            Console.ReadKey();
+        }
+    }
+}
